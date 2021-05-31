cfada545c0c3a8a59025e6c9d4d563d6ae3a666a
Grivn Wang <grivn.wang@gmail.com>
2021-05-31 10:14:51 +0800

    feat(*): interface conversion for btree

--- btree/btree_test.go
+++ btree/btree_test.go
@@ -15,6 +15,24 @@ func (m *MyTree) Less(item btree.Item) bool {
 	return m.Age < (item.(*MyTree)).Age
 }
 
+type MyTree1 struct {
+	Age  int
+	Name string
+}
+
+func (m *MyTree1) Less(item btree.Item) bool {
+	return m.Age < (item.(*MyTree1)).Age
+}
+
+type MyTree2 struct {
+	Age  int
+	Point int
+}
+
+func (m *MyTree2) Less(item btree.Item) bool {
+	return m.Age < (item.(*MyTree2)).Age
+}
+
 func TestDescendRange(t *testing.T) {
 	tree := btree.New(2) //创建一个2-3-4 树
 	for i := 0; i < 100; i++ {
@@ -39,4 +57,27 @@ func TestDescendRange(t *testing.T) {
 		fmt.Println(item)
 		return true
 	})
+}
+
+func TestCon(t *testing.T) {
+	tree := btree.New(2) //创建一个2-3-4 树
+	for i := 0; i < 100; i++ {
+		//插入数据
+		tree.ReplaceOrInsert(&MyTree1{Age: i, Name: "freedom" + strconv.Itoa(i)})
+	}
+	value1 := tree.Min()
+
+	v1 := value1.(*MyTree1)
+
+	fmt.Println(v1)
+
+	for i := 0; i < 100; i++ {
+		//插入数据
+		tree.ReplaceOrInsert(&MyTree2{Age: i, Point: i*10})
+	}
+	value2 := tree.Min()
+
+	v2 := value2.(*MyTree2)
+
+	fmt.Println(v2.Age)
 }
