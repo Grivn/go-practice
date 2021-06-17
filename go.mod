165d48842b8f5a1e3d60045b63ab682f1b22ed7a
Grivn Wang <grivn.wang@gmail.com>
2021-06-17 18:54:23 +0800

    feat(logrus): how to init logrus and print logs into files or stdout

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/Grivn/go-practice
 go 1.15
 
 require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
 	github.com/google/btree v1.0.0
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.0
 	github.com/stretchr/testify v1.3.0
+
 )
---
+++ logrus/rus_test.go
@@ -0,0 +1,25 @@
+package logrus
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+	"time"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/sirupsen/logrus"
+)
+
+func TestLogger2(t *testing.T) {
+	writer1 := &bytes.Buffer{}
+	writer2 := os.Stdout
+	writer3, err := os.OpenFile("replica_"+time.Now().Format("2006-01-02_15:04:05")+".log", os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		logrus.Fatalf("create file log.txt failed: %v", err)
+	}
+	logrus.SetFormatter(&nested.Formatter{NoColors: true})
+
+	logrus.SetOutput(io.MultiWriter(writer1, writer2, writer3))
+	logrus.Info("info msg")
+}
