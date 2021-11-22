6ef95b2617e02d65db427278238b8aae217f9d18
Grivn Wang <grivn.wang@gmail.com>
2021-11-22 15:29:27 +0800

    feat(*): metrics channel

--- channels/chann_test.go
+++ channels/chann_test.go
@@ -1,6 +1,7 @@
 package channels
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 )
@@ -35,4 +36,48 @@ func TestChannels(t *testing.T) {
 		wg.Done()
 	}()
 	wg.Wait()
+}
+
+func TestDChannels(t *testing.T) {
+	ch := make(chan bool)
+
+	var wg sync.WaitGroup
+
+	closeC := make(chan bool)
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-closeC:
+				fmt.Println("closed coroutine")
+				return
+			case <-ch:
+				fmt.Println("test line")
+			}
+		}
+	}()
+
+	go func() {
+		ch <- true
+		fmt.Println("send finished")
+		//wg.Done()
+	}()
+
+	drainChannel(ch)
+	close(closeC)
+	wg.Wait()
+}
+
+func drainChannel(ch chan bool) {
+DrainLoop:
+	for {
+		select {
+		case <-ch:
+		default:
+			break DrainLoop
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/Grivn/go-practice
 go 1.15
 
 require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/antonfisher/nested-logrus-formatter v1.3.1
 	github.com/google/btree v1.0.0
+	github.com/prometheus/client_golang v0.9.3
+	github.com/shirou/gopsutil v3.21.9+incompatible
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.0
